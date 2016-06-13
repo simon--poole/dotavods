823464e6b5722225670485a9f82401cbb53f5e5d
Simon Poole <simon__poole@hotmail.com>
2016-06-13 01:08:22 +0100

    Manila major updates

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ var cssbeautify = require('gulp-cssbeautify');
 var gulpif = require('gulp-if');
 var sprity = require('sprity');
 var sprityless = require('sprity-less');
-var lwip = require('gulp-lwip');
+var gm = require('gulp-gm');
 var gutil = require('gulp-util');
 
 //Default task - watches
@@ -19,63 +19,66 @@ gulp.task('default', ['build']);
 //Build umbrella task
 gulp.task('build', ['minify-css']);
 
-gulp.task('clean', function(){
+gulp.task('clean', function () {
 	return gulp.src('dist/')
-        .pipe(clean());
+		.pipe(clean());
 });
 
-gulp.task('clean-team-icons', function(){
+gulp.task('clean-team-icons', function () {
 	return gulp.src('./teams/dist')
-        .pipe(clean());
+		.pipe(clean());
 });
-gulp.task('resize-team-icons', ['clean-team-icons'], function(){
+gulp.task('resize-team-icons', ['clean-team-icons'], function () {
 	return gulp.src(["./teams/**/*.png", "!./teams/dist/*.png"])
-		.pipe(lwip
-			.resize(48, 20)
-			.exportAs("png")
-		)
+		.pipe(gm(function (gmfile) {
+			return gmfile.resize(48, 20);
+		}))
 		.pipe(gulp.dest("./teams/dist"));
 });
 
 gulp.task('generate-team-icons', ['resize-team-icons'], function () {
-  return sprity.src({
-    src: './teams/dist/**/*.png',
-    style: './less/team-sprites.less',
-    name: 'teams',
-    processor: 'less',
-	margin: 0,
-  })
-   .on('error', gutil.log)
-  .pipe(gulpif('*.png', gulp.dest('./images/'), gulp.dest('./less/')))
+	return sprity.src({
+			src: './teams/dist/**/*.png',
+			style: './less/team-sprites.less',
+			name: 'teams',
+			processor: 'less',
+			margin: 0,
+			engine: 'sprity-gm'
+		})
+		.on('error', gutil.log)
+		.pipe(gulpif('*.png', gulp.dest('./images/'), gulp.dest('./less/')))
 });
 
-gulp.task('generate-thumbnails',function () {
-  return sprity.src({
-    src: './thumbs/**',
-    style: './less/thumb-sprites.less',
-    name: 'thumbnails',
-    processor: 'less',
-	margin: 0,
-  })
-   .on('error', gutil.log)
-  .pipe(gulpif('*.png', gulp.dest('./images/'), gulp.dest('./less/')))
+gulp.task('generate-thumbnails', function () {
+	return sprity.src({
+			src: './thumbs/**',
+			style: './less/thumb-sprites.less',
+			name: 'thumbnails',
+			processor: 'less',
+			margin: 0,
+			engine: 'sprity-gm'
+		})
+		.on('error', gutil.log)
+		.pipe(gulpif('*.png', gulp.dest('./images/'), gulp.dest('./less/')))
 });
 
 gulp.task('sprites', ['generate-team-icons', 'generate-thumbnails']);
 
-gulp.task('less-build', ['clean'], function() {
-    	return gulp.src('less/dotavods.less')
-	        .pipe(less())
+gulp.task('less-build', ['clean'], function () {
+	return gulp.src('less/dotavods.less')
+		.pipe(less())
 		.pipe(cssbeautify({
-	            indent: '	',
-	            openbrace: 'separate-line',
-	            autosemicolon: true
-	        }))
-	        .pipe(gulp.dest('dist/'));
+			indent: '	',
+			openbrace: 'separate-line',
+			autosemicolon: true
+		}))
+		.pipe(gulp.dest('dist/'));
 });
-gulp.task('minify-css', ['less-build'], function(){
+gulp.task('minify-css', ['less-build'], function () {
 	return gulp.src('dist/dotavods.css')
 		.pipe(cssmin())
-		.pipe(rename({suffix: '.min'}))
+		.pipe(rename({
+			suffix: '.min'
+		}))
 		.pipe(gulp.dest('dist/'));
 });
